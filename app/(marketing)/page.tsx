384b89c03af45981213b9f4682fac74b45413b02
maisylattanavong <maisylattanavong@gmail.com>
2024-08-09 16:55:13 +0700

    update

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -9,6 +9,11 @@ export default function Home() {
       <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
         <Image src='/hero1.png' fill alt="Hero" />
       </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, Practice, and master new languages with Lingo
+        </h1>
+      </div>
     </div>
   );
 }
