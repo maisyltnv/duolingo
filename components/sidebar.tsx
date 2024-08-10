50e38f020b5f53d6d23388a0f56c47b75a16ad5f
maisylattanavong <maisylattanavong@gmail.com>
2024-08-10 10:15:56 +0700

    update

---
+++ components/sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div className='bg-blue-500 h-full w-[256px]'>
+            Sidebar
+        </div>
+    )
+}
+
+export default Sidebar
