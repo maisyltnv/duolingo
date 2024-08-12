2ae92272b59b6cf6ec5ef515286d876dc2891e7f
maisylattanavong <maisylattanavong@gmail.com>
2024-08-12 13:47:23 +0700

    update

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,16 +1,23 @@
 import FeedWrapper from '@/components/feed-wrapper'
 import StrickyWrapper from '@/components/sticky-wrapper'
 import React from 'react'
+import Header from './header'
+import UserProgress from '@/components/user-progress'
+
 
 const LearnPage = () => {
     return (
         <div className='flex flex-row-reverse gap-[48px] px-6'>
             <StrickyWrapper>
-                My sticky sidebar
+                <UserProgress 
+                activeCourse={{title:"Vietnamese", imageSrc:"/vietFlag.png"}}
+                hearts={5}
+                points={100}
+                hasActiveSubscription={false}/>
             </StrickyWrapper>
 
             <FeedWrapper>
-                <Header title="Spanish" />
+                <Header title="Vietnamese" />
             </FeedWrapper>
         </div>
     )
